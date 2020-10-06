5165495cfadabf0f19090e9b3e43e4dd037a8e11
lesenelir <miaomiaobabyzy@gmail.com>
2020-10-06 13:58:18 +0800

    edit

---
+++ MatplotlibYWY/plt06-pyplot-loadcsv.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file_name = './../Numpy&PandasYWY/csv_file/TestPandas.csv'
+
+x, y = pd.read_csv(file_name, delimiter=',', unpack=True)
+plt.plot(x, y)
+
+plt.show()
