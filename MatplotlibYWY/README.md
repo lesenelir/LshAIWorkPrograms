a32d79936c9327a2c607a3e4ccfe428c94fb231b
lesenelir <miaomiaobabyzy@gmail.com>
2020-10-06 13:04:44 +0800

    edit

--- MatplotlibYWY/README.md
+++ MatplotlibYWY/README.md
@@ -1,1 +1,9 @@
 matplotlib版本 3.0.3
+
+### pyplot intro
+    plt.plot([x坐标1, x坐标2], [y坐标1, y坐标2])
+    plt.xlabel()
+    plt.ylabel()
+    plt.title()
+    plt.legend() 显示图例，此时要配合plot中的label使用
+    plt.show()  
---
+++ MatplotlibYWY/plt01-pyplot-intro.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+x1 = [1, 2, 3]
+y1 = [5, 7, 4]
+
+x2 = [1, 2, 3]
+y2 = [10, 14, 12]
+
+# draw a pic
+plt.plot(x1, y1, label='First Line')
+plt.plot(x2, y2, label='Second Line')
+
+# add text description
+plt.xlabel('plot number')
+plt.ylabel('important var')
+plt.title('Simple Test Graph')
+
+plt.legend()
+plt.show()
+
