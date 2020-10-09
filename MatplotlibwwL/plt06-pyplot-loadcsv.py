31d2afc6d8bfc2beb78679c4b563d446ed89d693
lesenelir <miaomiaobabyzy@gmail.com>
2020-10-09 17:57:48 +0800

    format

--- MatplotlibwwL/plt06-pyplot-loadcsv.py
+++ MatplotlibwwL/plt06-pyplot-loadcsv.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file_name = '../Numpy&PandaswLw/csv_file/TestPandas.csv'
+file_name = '../Numpy&PandaswwL/csv_file/TestPandas.csv'
 
 x, y = pd.read_csv(file_name, delimiter=',', unpack=True)
 plt.plot(x, y)
